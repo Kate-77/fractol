485807c90a43b714614ab254079e62486bd3e537
Kaoutar Moutaouakil <kmoutaou@e1r8p3.1337.ma>
2022-02-15 04:53:56 +0100

    A lil bit of hope

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kmoutaou <kmoutaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 21:58:30 by kmoutaou          #+#    #+#             */
-/*   Updated: 2022/02/10 22:45:00 by kmoutaou         ###   ########.fr       */
+/*   Updated: 2022/02/15 04:52:10 by kmoutaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include <stdio.h>
+#include <stdlib.h>
+
 
 typedef struct	s_data {
 	void	*img;
@@ -38,7 +40,10 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 int	key_hook(int keycode, t_vars *vars)
 {
 	if (keycode == 53)
+	{
 		mlx_destroy_window(vars->mlx, vars->win);
+		exit(0);
+	}
 	return (0);
 }
 
@@ -52,14 +57,18 @@ int	mouse_hook(int mousecode, t_vars *vars)
 	return (0);
 }
 
+double	ft_color(int number)
+{
+	int	color;
+
+	return (color);
+}
+
 int	main()
 {
 	t_vars	vars;
-	// void	*mlx;
-	// void	*mlx_win;
 	t_data	img;
 	int		i = 0;
-	float	c;
 	// float	x_min = -2.1;
 	// float	x_max = 0.6;
 	// float	y_min = -1.2;
@@ -71,66 +80,94 @@ int	main()
 	double 	cr = 0;
 	double	ci = 0;
 	double	tmp = 0;
+	int		color;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1000, 1000, "FRACTOL");
-	img.img = mlx_new_image(vars.mlx, 1000, 1000);
+	vars.win = mlx_new_window(vars.mlx, 1000, 900, "FRACTOL");
+	img.img = mlx_new_image(vars.mlx, 1000, 900);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,&img.endian);
 
-	while (x < 1000 && y < 1000)
+	// while (x < 1500 && y < 1500)
+	// {
+	// 	cr = (x - 1500.0/2.0)*4.0/1500.0;
+	// 	ci = (y - 1500.0/2.0)*4.0/1500.0;
+	// 	printf("cr = %f \n",cr);
+	// 	printf("ci = %f\n",ci);
+	// 	zr = 0;
+	// 	zi = 0;
+	// 	i = 0;
+	// 	while (zr*zr + zi*zi <= 4 && i < 100)
+	// 	{
+	// 		//my_mlx_pixel_put(&img, x, y, 0xDDA0DD);
+	// 		if (zr*zr > 2 || zi*zi > 2) // ila kan kharej range or the M set => display it
+	// 		{
+	// 			my_mlx_pixel_put(&img, x, y, 0x00FFFFFF);
+	// 		// 	printf("zr = %f \n",zr);
+	// 		// 	printf("zi = %f\n",zi);
+	// 		}
+			
+	// 		// else
+	// 		// {
+	// 		// 	my_mlx_pixel_put(&img, x, y, 0x00010101);
+	// 		// }
+	// 		tmp = zr;
+	// 		zr = zr*zr - zi*zi + cr;
+	// 		zi = 2 * zi * tmp + ci;		
+	// 		i++;
+	// 	}
+		
+	// 	// my_mlx_pixel_put(&img, x, y, 0x00A86BF0);
+	// 	// my_mlx_pixel_put(&img, (int)zr, (int)zi, 0x00A86BF0);
+	// 	if (zr*zr > 2 || zi*zi > 2) // ila kan kharej range or the M set => display it
+	// 	{
+	// 		my_mlx_pixel_put(&img, (int)zr, (int)zi, 0x00FFFFFF);
+	// 		my_mlx_pixel_put(&img, x, y, 0x00FFFFFF);
+	// 		printf("zr = %f \n",zr);
+	// 		printf("zi = %f\n",zi);
+	// 	}
+	// 	else
+	// 	{
+	// 		my_mlx_pixel_put(&img, (int)zr, (int)zi, 0x00010101);
+	// 		my_mlx_pixel_put(&img, x, y, 0x00010101);
+	// 	}
+	// 	x++;
+	// 	y++;
+	// }
+
+	while (y < 900)
 	{
-		cr = (x - 1000/2.0)*4.0/1000;
-		ci = (y - 1000/2.0)*4.0/1000;
-		printf("cr = %f \n",cr);
-		printf("ci = %f\n",ci);
-		zr = 0;
-		zi = 0;
-		i = 0;
-		while (zr*zr+zi*zi <= 4 && i < 100)
+		x = 0;
+		while (x < 1000)
 		{
-			tmp = zr;
-			zr = zr*zr - zi*zi + cr;
-			zi = 2 * zi * tmp + ci;
-			if (zr*zr+zi*zi <= 4) // ila kan dakhel range or the M set => display it w/ white
+			cr = (x - 1000.0/2.0)*4.0/1000.0;
+			ci = (y - 900.0/2.0)*4.0/1000.0;
+			zr = 0;
+			zi = 0;
+			i = 0;
+			while (zr*zr + zi*zi <= 4 && i < 100)
 			{
-				my_mlx_pixel_put(&img, x, y, 0x0000FF00);
-				printf("zr = %f \n",zr);
-				printf("zi = %f\n",zi);
+				tmp = zr;
+				zr = zr*zr - zi*zi + cr;
+				zi = 2 * zi * tmp + ci;	
+					
+				if (zr*zr + zi*zi > 4) // ila kan kharej range or the M set => display it
+				{
+					color = 255 - i * 255 / 100;
+					//color = ft_color();
+					my_mlx_pixel_put(&img, x, y, color);
+				}
+				else
+				{
+					my_mlx_pixel_put(&img, x, y, 0x000000);
+				}
+				i++;
 			}
-			if (zr*zr+zi*zi > 4) // ila kan kharej range or the M set => display it
-			{
-				my_mlx_pixel_put(&img, x, y, 0x00FFFFFF);
-				printf("zr = %f \n",zr);
-				printf("zi = %f\n",zi);
-			}
-			i++;
+			
+			x++;
 		}
-		x++;
 		y++;
 	}
 
-	// while (i < 100 && zr*zr + zi*zi <= 4)
-	// {
-	// 	tmp = zr;
-	// 	zr = zr*zr - zi*zi + cr;
-    //     zi = 2 * zi * tmp + ci;
-	// 	my_mlx_pixel_put(&img, zr, zi, 0x0000FF00);
-	// 	i++;
-	// }
-
-// // cercle
-// 	int	r = 50;
-// 	int	x = 100;
-// 	int	y = 50;
-
-// 	while (y)
-// 	{
-// 		if (y - x == r)
-// 			my_mlx_pixel_put(&img, x, y, 0x0000FF00);	
-// 		y--;
-// 	}
-
-	
 	// i = 70;
 	// j = 20;
 	// while (i >= 20)
