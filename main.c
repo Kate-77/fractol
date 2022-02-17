e27279f663d5d4361deeaea77a14231be8a41fad
Kaoutar Moutaouakil <kmoutaou@e1r1p4.1337.ma>
2022-02-17 22:39:53 +0100

    Chwya wkan

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kmoutaou <kmoutaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 21:58:30 by kmoutaou          #+#    #+#             */
-/*   Updated: 2022/02/15 04:52:10 by kmoutaou         ###   ########.fr       */
+/*   Updated: 2022/02/17 22:38:39 by kmoutaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,13 +47,36 @@ int	key_hook(int keycode, t_vars *vars)
 	return (0);
 }
 
-int	mouse_hook(int mousecode, t_vars *vars)
+double zoom(double x, double x0, double zoom)
 {
-	printf("mousecode : %d\n", mousecode);
-	// if (mousecode == 5)
-	// 	ZOOM_IN
-	// else if (mousecode == 4)
-	// 	ZOOM_OUT
+	return (x + ((x0 - x) * zoom));
+}
+
+double	applyZoom(double mouse_x, double mouse_y, double zoom, double *x_min, double *x_max, double *y_min, double *y_max)
+{
+	double zooming = 1.0 / zoom;
+
+    *x_min = zoom(mouse_x, x_min, zooming);
+    *y_min = zoom(mouse_y, y_min, zooming);
+    *x_max = zoom(mouse_x, x_max, zooming);
+    *y_max = zoom(mouse_y, y_max, zooming);
+}
+
+int	mouse_hook(int mousecode, int x, int y, t_vars *vars, double *x_min, double *x_max, double *y_min, double *y_max)
+{
+	//printf("mousecode : %d\n", mousecode);
+	//printf ("x = %d\n y = %d\n", x, y);
+	if (mousecode == 5) // zoom in
+	{
+		applyZoom(x, y, 0.9, x_min, x_max, y_min, y_max);
+	}
+	else if (mousecode == 4) // zoom out
+	{
+		applyZoom(x, y, -0.1, x_min, x_max, y_min, y_max);
+	}
+	else
+		applyZoom(x, y, 1, x_min, x_max, y_min, y_max);
+
 	return (0);
 }
 
@@ -69,10 +92,19 @@ int	main()
 	t_vars	vars;
 	t_data	img;
 	int		i = 0;
-	// float	x_min = -2.1;
-	// float	x_max = 0.6;
-	// float	y_min = -1.2;
-	// float	y_max = 1.2;
+	int		max_i = 100;
+	double	x_min = -2.0;
+	double	x_max = 2.0;
+	double	y_min = -2.0;
+	double	y_max = 2.0;
+	int		height = 1000;
+	int		width = 1000;
+	double	PixelWidth;
+	double	PixelHeight;
+	//int		*color;
+	// double	PixelWidth=(x_max-x_min)/width /2;
+    // double	PixelHeight=(y_max-y_min)/height / 2;
+	double	cx, cy;
 	int		x = 0;
 	int		y = 0;
 	double	zr = 0;
@@ -81,84 +113,75 @@ int	main()
 	double	ci = 0;
 	double	tmp = 0;
 	int		color;
+	int	ms = mouse_hook(0, 0, 0, &vars, &x_min, &x_max, &y_min, &y_max);
+
+
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1000, 900, "FRACTOL");
-	img.img = mlx_new_image(vars.mlx, 1000, 900);
+	vars.win = mlx_new_window(vars.mlx, width, height, "FRACTOL");
+	img.img = mlx_new_image(vars.mlx, width, height);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,&img.endian);
 
-	// while (x < 1500 && y < 1500)
+	// while (y < height)
 	// {
-	// 	cr = (x - 1500.0/2.0)*4.0/1500.0;
-	// 	ci = (y - 1500.0/2.0)*4.0/1500.0;
-	// 	printf("cr = %f \n",cr);
-	// 	printf("ci = %f\n",ci);
-	// 	zr = 0;
-	// 	zi = 0;
-	// 	i = 0;
-	// 	while (zr*zr + zi*zi <= 4 && i < 100)
+	// 	x = 0;
+	// 	while (x < width)
 	// 	{
-	// 		//my_mlx_pixel_put(&img, x, y, 0xDDA0DD);
-	// 		if (zr*zr > 2 || zi*zi > 2) // ila kan kharej range or the M set => display it
+	// 		cr = (x - 1500.0/2.0)*4.0/1500.0;
+	// 		ci = (y - 1500.0/2.0)*4.0/1500.0;
+	// 		zr = 0;
+	// 		zi = 0;
+	// 		i = 0;
+	// 		while (zr*zr + zi*zi <= 4 && i < 100)
 	// 		{
-	// 			my_mlx_pixel_put(&img, x, y, 0x00FFFFFF);
-	// 		// 	printf("zr = %f \n",zr);
-	// 		// 	printf("zi = %f\n",zi);
+	// 			tmp = zr;
+	// 			zr = zr*zr - zi*zi + cr;
+	// 			zi = 2 * zi * tmp + ci;		
+	// 			i++;
 	// 		}
-			
-	// 		// else
-	// 		// {
-	// 		// 	my_mlx_pixel_put(&img, x, y, 0x00010101);
-	// 		// }
-	// 		tmp = zr;
-	// 		zr = zr*zr - zi*zi + cr;
-	// 		zi = 2 * zi * tmp + ci;		
-	// 		i++;
+	// 		if (i < 100) // ila kan kharej range or the M set => display it
+	// 		{
+	// 			color = 255 - i * 255 / 100;
+	// 			my_mlx_pixel_put(&img, x, y, color);
+	// 		}
+	// 		else
+	// 		{
+	// 			my_mlx_pixel_put(&img, x, y, 0x000000);
+	// 		}
+	// 		x++;
 	// 	}
-		
-	// 	// my_mlx_pixel_put(&img, x, y, 0x00A86BF0);
-	// 	// my_mlx_pixel_put(&img, (int)zr, (int)zi, 0x00A86BF0);
-	// 	if (zr*zr > 2 || zi*zi > 2) // ila kan kharej range or the M set => display it
-	// 	{
-	// 		my_mlx_pixel_put(&img, (int)zr, (int)zi, 0x00FFFFFF);
-	// 		my_mlx_pixel_put(&img, x, y, 0x00FFFFFF);
-	// 		printf("zr = %f \n",zr);
-	// 		printf("zi = %f\n",zi);
-	// 	}
-	// 	else
-	// 	{
-	// 		my_mlx_pixel_put(&img, (int)zr, (int)zi, 0x00010101);
-	// 		my_mlx_pixel_put(&img, x, y, 0x00010101);
-	// 	}
-	// 	x++;
 	// 	y++;
 	// }
-
-	while (y < 900)
+		
+	while (y < height)
 	{
+		//cy = y_min + y * PixelHeight;
 		x = 0;
-		while (x < 1000)
+		while (x < width)
 		{
-			cr = (x - 1000.0/2.0)*4.0/1000.0;
-			ci = (y - 900.0/2.0)*4.0/1000.0;
+			//cx = x_min + x * PixelWidth;
+			//cr = (x - 1000.0/2.0)*4.0/1000.0;
+			//ci = (y - 900.0/2.0)*4.0/1000.0;
+			cx= (x / (width / (x_max-x_min)) + x_min);
+    		cy= (y / (height / (y_max-y_min)) + y_min);
 			zr = 0;
 			zi = 0;
 			i = 0;
-			while (zr*zr + zi*zi <= 4 && i < 100)
+			while (zr*zr + zi*zi <= 4 && i < max_i)
 			{
 				tmp = zr;
-				zr = zr*zr - zi*zi + cr;
-				zi = 2 * zi * tmp + ci;	
-					
-				if (zr*zr + zi*zi > 4) // ila kan kharej range or the M set => display it
+				zr = zr*zr - zi*zi + cx;
+				zi = 2 * zi * tmp + cy;					
+				if (zr*zr + zi*zi >= 4) // ila kan kharej range or the M set => display it
 				{
-					color = 255 - i * 255 / 100;
-					//color = ft_color();
+					color = i * 255 / max_i;
+					//color = 255 - i * 255 / max_i;
+					//printf("color = %d\n", color);
 					my_mlx_pixel_put(&img, x, y, color);
 				}
 				else
 				{
-					my_mlx_pixel_put(&img, x, y, 0x000000);
+					my_mlx_pixel_put(&img, x, y, 0xFFFFFF);
 				}
 				i++;
 			}
